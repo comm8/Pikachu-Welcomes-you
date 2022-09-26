eb0ffb5074223bacc32f384543223348e740e30c
Comm8 <elliotcomeau@outlook.com>
2022-09-26 13:18:44 -0700

    pika pika

---
+++ src/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Pikachu welcomes you to the world of pokemon\n\n (\\__/)\n (o^.^)\nz(_(\")(\")");
+    }
+}
